b4c099fd2430fdb00190b2781d7e0a6a23bcc85d
karivarkey <geevargheseregi78@gmail.com>
2026-02-26 18:49:53 +0530

    feat: update chat_id in Fetch Conversations and add CORS support

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import chatRouter from "./src/features/chat/chat.router";
 import memoryRouter from "./src/features/memory/memory.router";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", chatRouter);
 app.use("/api", memoryRouter);
